1ec0a3b87382da58fbac1d40dd75b112b0cf531c
Benjamin <Benjamin@192.168.1.127>
2017-04-02 18:55:57 -0500

    First commit.

---
+++ src/parkinggarage/studentsolution/Startup.java
@@ -0,0 +1,22 @@
+package parkinggarage.studentsolution;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Startup {
+    public static void main(String[] args) {
+        /** What's in a parking garage?
+         *  There's two automated ticket machines
+         * One for checkin and one for checkout
+         * // PROCESS BEGIN //
+         * The checkin machine gives a ticket which indicates checkin time and date
+         * When you are ready to leave, you give the checkout machine the ticket and calculates how long you parked and how much you owe
+         * These values are displayed on a electronic display in the automated checkout machine
+         * Once the fee is paid, a receipt is given
+         * // PROCESS END //
+         *  */
+        //We'll need some objects to work with here
+        
+    }
+}
